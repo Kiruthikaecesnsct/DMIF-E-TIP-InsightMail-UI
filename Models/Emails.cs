68f777179c1897221a565c8f8ce1ab2a148b41f0
kiruthikar <kiruthika.r@wisework.in>
2026-03-12 21:58:48 +0530

    Initial commit

---
+++ Models/Emails.cs
@@ -0,0 +1,27 @@
+﻿namespace InsightMailUI.Models
+{
+    public class Email
+    {
+        public string Id { get; set; }
+
+        public string Sender { get; set; }
+
+        public List<string> Recipients { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime ReceivedDate { get; set; }
+
+        public string Category { get; set; }
+
+        public int? Priority { get; set; }
+
+        public string ClassificationReasoning { get; set; }
+
+        public double? ClassificationConfidence { get; set; }
+
+        public DateTime? ClassifiedDate { get; set; }
+    }
+}
