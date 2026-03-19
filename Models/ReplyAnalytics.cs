0be38fc9f99192c2a74cf79595f76e7d62793212
kiruthikar <kiruthika.r@wisework.in>
2026-03-19 18:53:22 +0530

    assignment 9 done, analytics added

---
+++ Models/ReplyAnalytics.cs
@@ -0,0 +1,22 @@
+﻿namespace InsightMailUI.Models
+{
+    public class ReplyAnalyticsEntry
+    {
+        public string EmailId { get; set; } = "";
+        public string SelectedType { get; set; } = "";
+        public string OriginalBody { get; set; } = "";
+        public string FinalBody { get; set; } = "";
+        public string ToneUsed { get; set; } = "";
+        public int MaxWordsUsed { get; set; }
+    }
+
+    public class AnalyticsSummary
+    {
+        public int TotalGenerated { get; set; }
+        public int BriefSelected { get; set; }
+        public int DetailedSelected { get; set; }
+        public int DeclineSelected { get; set; }
+        public double AverageEditDistance { get; set; }
+        public string MostUsedTone { get; set; } = "";
+    }
+}
