0d07bc67ff4a41cc546cb504447a8d6e8f9a4e2e
kiruthikar <kiruthika.r@wisework.in>
2026-03-19 15:09:57 +0530

    week 9 session 9 implemented

---
+++ Models/ReplyResponse.cs
@@ -0,0 +1,19 @@
+﻿// Models/ReplyResponse.cs
+namespace InsightMailUI.Models
+{
+    public class ReplyResponse
+    {
+        public string EmailId { get; set; } = "";
+        public DateTime GeneratedAt { get; set; }
+        public List<EmailReply> Replies { get; set; } = new();
+    }
+
+    public class EmailReply
+    {
+        public string Type { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Body { get; set; } = "";
+        public string Tone { get; set; } = "";
+        public double Confidence { get; set; }
+    }
+}
